168e64bd60c1b999c5421d394d8d6d270ef89e8c
nikenhpsr <nikenhpsr@users.noreply.github.com>
2023-02-26 15:03:48 +0700

    automated commit by check50 [check50=True]

---
+++ mario.c
@@ -0,0 +1,30 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    // Get input to represent height
+    int n;
+    do
+    {
+        n = get_int("Height: ");
+    }
+    while (n < 1 || n > 8);
+
+    // Print how many height (i unit)
+
+    for (int i = 0; i < n; i++)
+    {
+    // Print width
+
+        for (int j = 0; j < n + i + 3; j++)
+        {
+            if (j == n || j == n+1 || i + j < n-1)
+                printf(" ");
+            else
+                printf("#");
+        }
+        printf("\n");
+    }
+
+}
